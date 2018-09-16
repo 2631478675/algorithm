692dd97e34a24fe3acef19b068c55eaa27fdb5e8
liuyuanlin <2631478675@qq.com>
2018-09-16 23:11:49 +0800

    get on sort

--- sort/heapSort/main.cpp
+++ sort/heapSort/main.cpp
@@ -1,16 +1,13 @@
 #include <iostream>
-
-using namespace std;
 /**
  * http://www.cnblogs.com/skywang12345/p/3602162.html
- * @param R
- * @param i
- * @param len
  */
+using namespace std;
+
 void HeapDown(int *R, int i, int len) {
     int child = i * 2 + 1;
     int current = i;
-    for (; child < len ; current = child , child = child * 2 + 1) {
+    for (; child < len; current = child, child = child * 2 + 1) {
         // it has rightChild and who bigger
         if (child + 1 < len && R[child] < R[child + 1]) {
             child++;
@@ -29,28 +26,48 @@ void HeapBuild(int *R, int len) {
     for (; i >= 0; i--) {
         HeapDown(R, i, len);
     }
-    for (i = len - 1; i > 0; i--)
-    {
-        // 交换a[0]和a[i]。交换后，a[i]是a[0...i]中最大的。
+//    for (i = len - 1; i > 0; i--)
+//    {
+//        // 交换a[0]和a[i]。交换后，a[i]是a[0...i]中最大的。
+//        swap(R[0], R[i]);
+//        // 调整a[0...i-1]，使得a[0...i-1]仍然是一个最大堆。
+//        // 即，保证a[i-1]是a[0...i-1]中的最大值。
+//        HeapDown(R, 0, i);
+//    }
+}
+
+void HeapSort(int *R, int len) {
+    for (int i = len - 1; i > 0; i--) {
         swap(R[0], R[i]);
-        // 调整a[0...i-1]，使得a[0...i-1]仍然是一个最大堆。
-        // 即，保证a[i-1]是a[0...i-1]中的最大值。
         HeapDown(R, 0, i);
     }
 }
 
 int main() {
     int i;
-    int a[] = {20,30,90,40,70,110,60,10,100,50,80};
+    int a[] = {20, 30, 90, 40, 70, 110, 60, 10, 100, 50, 80};
     int ilen = (sizeof(a)) / (sizeof(a[0]));
+
+
     cout << "before sort:";
-    for (i=0; i<ilen; i++)
+    for (i = 0; i < ilen; i++)
         cout << a[i] << " ";
     cout << endl;
+
+
     HeapBuild(a, ilen);            // 升序排列
+    cout << "build  heap:";
+    for (i = 0; i < ilen; i++)
+        cout << a[i] << " ";
+    cout << endl;
+
+
     //heapSortDesc(a, ilen);        // 降序排列
+    
+
+    HeapSort(a, ilen);
     cout << "after  sort:";
-    for (i=0; i<ilen; i++)
+    for (i = 0; i < ilen; i++)
         cout << a[i] << " ";
     cout << endl;
     return 0;
