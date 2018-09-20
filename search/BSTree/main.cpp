2016459df96d4b7ab47df31edb0eeb85156f1a4b
deki <deki.liu@sap.com>
2018-09-20 14:45:28 +0800

    increase bstree

---
+++ search/BSTree/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "BSTree.h"
+
+using namespace std;
+
+static int arr[]= {1,5,4,3,2,6};
+#define TBL_size(a) ((sizeof(a))/(sizeof(a[0])))
+
+int main() {
+    int len;
+    len = TBL_size(arr);
+
+    BSTree<int> *tree = new BSTree<int>();
+
+    cout << "insert: ";
+    for (int i = 0; i < len; i++) {
+        cout << arr[i] << " ";
+        tree->insert(arr[i]);
+    }
+
+    cout << endl;
+    cout << "traversing preOrder: ";
+    tree->preOrder();
+
+
+    cout << endl;
+    cout << "traversing midOrder: ";
+    tree->midOrder();
+    return 0;
+}
