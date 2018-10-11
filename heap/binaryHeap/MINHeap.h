dd5cd324f9250f4d27601b5a85ffac178df15af0
deki <deki.liu@sap.com>
2018-10-11 16:59:00 +0800

    bug

--- heap/binaryHeap/MINHeap.h
+++ heap/binaryHeap/MINHeap.h
@@ -9,58 +9,46 @@
 #ifndef BINARYHEAP_MINHEAP_H
 #define BINARYHEAP_MINHEAP_H
 
-#include <iostream>
-
-using namespace std;
 
 template<class T>
-class MaxHeap {
-private:
-    T *mHeap;        // 数据
-    int mCapacity;    // 总的容量
-    int mSize;        // 实际容量
+class MinHeap{
+private: //仅限内部类调用
+    T * heap; //堆中存储的数据
+    int mSize;
+    int mCapacity;  //堆的容量
 
-private:
-    // 最大堆的向下调整算法
-    void filterDown(int start, int end);
-
-    // 最大堆的向上调整算法(从start开始向上直到0，调整堆)
-    void filterUp(int start);
-
+    void filterUp(int);
+    void filterDown(int,int);
 public:
-    MaxHeap();
+    MinHeap();
+    MinHeap(int);
+    ~ MinHeap();
 
-    MaxHeap(int capacity);
-
-    ~MaxHeap();
-
-    // 返回data在二叉堆中的索引（索引从0开始）
-    int getIndex(T data);
-
-    // 删除最大堆中的data
-    int remove(T data);
-
-    // 将data插入到二叉堆中
-    int insert(T data);
-
-    // 打印二叉堆
     void print();
+    int insert(T data);
+    int deleteHeap(T data);
+    int getIndex(T data);
 };
 
 /**
- * 构造函数
+ * 无参构造函数
  * @tparam T
  */
 template<class T>
-MaxHeap<T>::MaxHeap() {
-    new(this)MaxHeap(30);
+MinHeap<T>::MinHeap() {
+    new(this)MinHeap(30);
 }
 
+/**
+ * 有参构造函数
+ * @tparam T
+ * @param capacity
+ */
 template<class T>
-MaxHeap<T>::MaxHeap(int capacity) {
+MinHeap<T>::MinHeap(int capacity) {
     mSize = 0;
     mCapacity = capacity;
-    mHeap = new T[mCapacity];
+    heap = new T[mCapacity];
 }
 
 /**
@@ -68,130 +56,98 @@ MaxHeap<T>::MaxHeap(int capacity) {
  * @tparam T
  */
 template<class T>
-MaxHeap<T>::~MaxHeap() {
-    mSize = 0;
+MinHeap<T>::~MinHeap() {
     mCapacity = 0;
-    delete[] mHeap;
+    mSize = 0;
+    delete [] heap;
 }
 
 /**
- * 最大堆的向下调整算法
+ * 获取data在数组中的索引
  * @tparam T
- * @param start 被下调节点的起始位置(一般为0，表示从第1个开始)
- * @param end 截至范围(一般为数组中最后一个元素的索引)
+ * @param data
+ * @return
  */
 template<class T>
-void MaxHeap<T>::filterDown(int start, int end) {
+int MinHeap<T>::getIndex(T data) {
+    int index = 0;
+    for(;index < mSize; index++){
+        if(data == heap[index]){
+            return index;
+        }
+    }
+    return -1;  //堆中没有这个数据
+}
+
+template<class T>
+void MinHeap<T>::filterUp(int start) {
     int current = start;
-    int left = current * 2 + 1;
-    T tmp = mHeap[current]; // ? ? ?
+    int parent = (current-1) / 2;
+    T valueOFCurrent = heap[current];
 
-    while (left <= end) {
-        if (left < end && mHeap[left] < mHeap[left + 1]) {
-            left++;
-        }
-        if (tmp >= mHeap[left]) {
-            break;
-        } else {
-            mHeap[current] = mHeap[left];
-            current = left;
-            left = 2 * left + 1;
-        }
-    }
-    mHeap[current] = tmp;
-}
-
-/**
- * 最大堆的向上调整算法
- * @tparam T
- * @param start 被上调节点的起始位置(一般为数组中最后一个元素的索引)
- */
-template<class T>
-void MaxHeap<T>::filterUp(int start) {
-    int current = start;            // 当前节点(current)的位置
-    int parent = (current - 1) / 2;        // 父(parent)结点的位置
-    T tmp = mHeap[current];        // 当前节点(current)的大小
-
-    while (current > 0) {
-        if (mHeap[parent] >= tmp)
-            break;
-        else {
-            mHeap[current] = mHeap[parent];
+    for(;current > 0;){
+        if(heap[parent] < heap[current]){
+            swap(heap[parent],heap[current]);
             current = parent;
-            parent = (parent - 1) / 2;
+            parent = (current-1)/2;
+        } else{
+            break;
         }
     }
-    mHeap[current] = tmp;
+
 }
 
-
-/**
- * 将data插入到二叉堆中
- * @tparam T
- * @param data
- * @return
- */
 template<class T>
-int MaxHeap<T>::insert(T data) {
-    // 如果"堆"已满，则返回
-    if (mSize == mCapacity)
+void MinHeap<T>::filterDown(int start, int end) {
+    int current = start;
+    int leftChildren = current*2 + 1;
+    T valueOFCurrent = heap[start];
+
+    while(leftChildren <= end){
+          if(leftChildren + 1 <= end && heap[leftChildren] > heap[leftChildren+1]){
+            leftChildren++;
+          }else if(valueOFCurrent <= heap[leftChildren]){
+            break;
+          } else{
+              swap(heap[current],heap[leftChildren]);
+              current = leftChildren;
+              leftChildren = current*2 +1;
+          }
+    }
+}
+
+template<class T>
+int MinHeap<T>::insert(T data) {
+    if(mSize = mCapacity){
+        cout << "the heap is full";
         return -1;
-
-    mHeap[mSize] = data;        // 将"数组"插在表尾
-    filterUp(mSize);    // 向上调整堆
-    mSize++;                    // 堆的实际容量+1
-
+    }
+    heap[mSize] = data;
+    filterUp(mSize);
+    mSize++;
     return 0;
 }
 
-/**
- * 删除数据data
- * @tparam T
- * @param data
- * @return
- */
 template<class T>
-int MaxHeap<T>::remove(T data) {
-    int index;
-    // 如果"堆"已空，则返回-1
-    if (mSize == 0)
+int MinHeap<T>::deleteHeap(T data) {
+    if(mSize == 0){
         return -1;
-
-    // 获取data在数组中的索引
-    index = getIndex(data);
-    if (index == -1)
+    }
+    int index = getIndex(data);
+    if(index == -1){
         return -1;
-
-    mHeap[index] = mHeap[--mSize];    // 用最后元素填补
-    filterDown(index, mSize - 1);        // 从index位置开始自上向下调整为最大堆
-
+    }
+    heap[index] = heap[--mSize];
+    filterDown(index,mSize-1);
     return 0;
 }
 
 
-/**
- * 返回data在二叉堆中的索引
- * @tparam T
- * @param data
- * @return
- */
 template<class T>
-int MaxHeap<T>::getIndex(T data) {
-    for (int i = 0; i < mSize; i++)
-        if (data == mHeap[i]) {
-            return i;
-        }
-    return -1;
+void MinHeap<T>::print() {
+    for (int i = 0; i < mSize ; ++i) {
+        cout << heap[i] << " ";
+    }
+    cout << endl;
 }
-
-/**
- * 打印二叉堆
- * @tparam T
- */
-template<class T>
-void MaxHeap<T>::print() {
-    for (int i = 0; i < mSize; i++)
-        cout << mHeap[i] << " ";
-}
-
 #endif //BINARYHEAP_MINHEAP_H
--- heap/binaryHeap/main.cpp
+++ heap/binaryHeap/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "MAXHeap.h"
 #include "MINHeap.h"
 
 int main()
@@ -6,29 +7,52 @@ int main()
     int a[] = {10, 40, 30, 60, 90, 70, 20, 50, 80};
     int i, len=(sizeof(a)) / (sizeof(a[0])) ;
     MaxHeap<int>* tree=new MaxHeap<int>();
+    MinHeap<int>* minTree = new MinHeap<int>();
 
-    cout << "== 依次添加: ";
+//    cout << "== 依次添加: ";
+//    for(i=0; i<len; i++)
+//    {
+//        cout << a[i] <<" ";
+//        tree->insert(a[i]);
+//    }
+//
+//    cout << "\n== 最 大 堆: ";
+//    tree->print();
+//
+//    i=85;
+//    tree->insert(i);
+//    cout << "\n== 添加元素: " << i;
+//    cout << "\n== 最 大 堆: ";
+//    tree->print();
+//
+//    i=90;
+//    tree->remove(i);
+//    cout << "\n== 删除元素: " << i;
+//    cout << "\n== 最 大 堆: ";
+//    tree->print();
+//    cout << endl;
+
+    cout << "== 依次添加min: ";
     for(i=0; i<len; i++)
     {
         cout << a[i] <<" ";
-        tree->insert(a[i]);
+        minTree->insert(a[i]);
     }
 
-    cout << "\n== 最 大 堆: ";
-    tree->print();
+    cout << "\n== 最 min 堆: ";
+    minTree->print();
 
     i=85;
-    tree->insert(i);
+    minTree->insert(i);
     cout << "\n== 添加元素: " << i;
-    cout << "\n== 最 大 堆: ";
-    tree->print();
+    cout << "\n== 最 min 堆: ";
+    minTree->print();
 
     i=90;
-    tree->remove(i);
+    minTree->deleteHeap(i);
     cout << "\n== 删除元素: " << i;
-    cout << "\n== 最 大 堆: ";
-    tree->print();
-    cout << endl;
+    cout << "\n== 最 min 堆: ";
+    minTree->print();
 
     return 0;
 }
